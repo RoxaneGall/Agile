bb03172ec6d5f0b64c22b5aedd75f079dde9ee30
RoxaneGall <gall.roxane@gmail.com>
2019-11-26 11:45:47 +0100

    methode chargerDemande

--- Deliveloo/src/main/java/Vue/Elements.java
+++ Deliveloo/src/main/java/Vue/Elements.java
@@ -8,16 +8,29 @@ import java.util.List;
 
 public class Elements {
 
+<<<<<<< Updated upstream
     /** Coordinates to set the map extent */
     private List<Coordinate> mapExtent;
 
     /** Coordinates du dépot et des points de livraison */
+=======
+    /** Elements nécessaires au plan */
+    private List<Coordinate> mapExtent = new ArrayList();
+
+    /** Elements nécessaire à la demande */
+>>>>>>> Stashed changes
     private Coordinate entrepot;
     private List<Pair<Coordinate,Coordinate>> deliveries;
 
 
+<<<<<<< Updated upstream
     public void setEntrepot(Intersection inter) {
         this.entrepot = inter.getCoordinate();
+=======
+
+    public void chargeDemande(Demande demande) {
+        entrepot = demande.getEntrepot().getCoordinate();
+>>>>>>> Stashed changes
     }
 
     public void addDelivery(Coordinate pickup, Coordinate deliver) {
