5abc854fb1cbb31d9aa9a8ca7bc83b017204be95
RoxaneGall <gall.roxane@gmail.com>
2019-11-26 11:52:57 +0100

    arrayList

--- Deliveloo/src/main/java/Vue/Controller.java
+++ Deliveloo/src/main/java/Vue/Controller.java
@@ -2,22 +2,26 @@ package Vue;
 
 import Modeles.Demande;
 import Modeles.Intersection;
+
 import com.sothawo.mapjfx.Coordinate;
+import com.sothawo.mapjfx.Extent;
 import javafx.util.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
 
     /** Elements pour le plan */
-    private List<Coordinate> mapExtent;
+    private Extent mapExtent;
 
     /** Elements pour la demande */
     private Coordinate entrepot;
     private List<Pair<Coordinate,Coordinate>> deliveries;
 
 
-    public void setMapExtent(List<Coordinate> planCoords) {
+    public void setMapExtent(ArrayList<Coordinate> planCoords) {
+        mapExtent = Extent.forCoordinates(planCoords);
 
     }
 
