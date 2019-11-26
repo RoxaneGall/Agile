ae95294f006673546a31d196b96a9744b7227129
manalelka <45950280+manalelka@users.noreply.github.com>
2019-11-26 10:55:21 +0100

    Update Controleur & Listener

--- Deliveloo/src/main/java/Controleur/Controleur.java
+++ Deliveloo/src/main/java/Controleur/Controleur.java
@@ -1,4 +1,10 @@
 package Controleur;
 
 public class Controleur {
+
+    public void chargerPlan(){
+        //appel méthode Alice qui renvoie les 4 points
+    }
+
+
 }
--- Deliveloo/src/main/java/Vue/Listener.java
+++ Deliveloo/src/main/java/Vue/Listener.java
@@ -1,4 +1,22 @@
 package Vue;
 
-public class Listener {
+import Controleur.Controleur;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Listener implements ActionListener {
+    private Controleur controleur;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "Charger Plan":
+                chargerPlan();
+        }
+
+    }
+
+    public void chargerPlan(){
+        controleur.chargerPlan();
+    }
 }
