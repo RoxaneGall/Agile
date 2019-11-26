365e4aa04aeb9c98092448631b71ccee3aac7211
manalelka <45950280+manalelka@users.noreply.github.com>
2019-11-26 11:36:53 +0100

    Update

--- Deliveloo/src/main/java/Controleur/Controleur.java
+++ Deliveloo/src/main/java/Controleur/Controleur.java
@@ -9,11 +9,11 @@ public class Controleur {
 
     private LectureXML lec;
 
-    public ArrayList<Coordinate>  chargerPlan( String chemin) throws Exception {
-        lec.chargerPlan(chemin);
+    public ArrayList<Coordinate>  chargerPlan( String path) throws Exception {
+        lec.chargerPlan(path);
         ArrayList<Coordinate> limites = new ArrayList<Coordinate>();
+        //limites= méthode Alice qui renvoie les 4 points
         return limites;
-        //appel méthode Alice qui renvoie les 4 points
     }
 
 
--- Deliveloo/src/main/java/Vue/Listener.java
+++ Deliveloo/src/main/java/Vue/Listener.java
@@ -1,19 +1,30 @@
 package Vue;
 
 import Controleur.Controleur;
+import com.sothawo.mapjfx.Coordinate;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
 
 public class Listener implements ActionListener {
     private Controleur controleur;
+    private JFileChooser choix = new JFileChooser();
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
             case "Charger Plan":
-                String chemin="";
+                String path="";
                 try {
-                    chargerPlan(chemin);
+                    choix.setCurrentDirectory(new File("./datas"));
+                    if (choix.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                        path = choix.getSelectedFile().getAbsolutePath();
+                        chargerPlan(path);
+                    }
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
@@ -21,7 +32,9 @@ public class Listener implements ActionListener {
 
     }
 
-    public void chargerPlan( String chemin) throws Exception {
-        controleur.chargerPlan(chemin);
+    public void chargerPlan( String path) throws Exception {
+        ArrayList<Coordinate> limites=new ArrayList<Coordinate>();
+        limites=controleur.chargerPlan(path);
+
     }
 }
