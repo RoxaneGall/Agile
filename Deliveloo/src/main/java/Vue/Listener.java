103d60b786cf69c6fd09c9f47c8b8112f7e3b1b6
manalelka <45950280+manalelka@users.noreply.github.com>
2019-11-26 15:19:16 +0100

    Update Listener.java

--- Deliveloo/src/main/java/Vue/Listener.java
+++ Deliveloo/src/main/java/Vue/Listener.java
@@ -3,6 +3,7 @@ package Vue;
 import Modeles.Demande;
 import Service.Service;
 import com.sothawo.mapjfx.Coordinate;
+import com.sothawo.mapjfx.event.MapViewEvent;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -15,17 +16,38 @@ public class Listener {
     private JFileChooser choix = new JFileChooser();
     private Controller controller;
 
-    private void setupEventHandlers() {
+    private void setOnActions() {
 
-
-
-
+        controller.chargerPlan.setOnAction(event -> {
+            String pathPlan = "";
+            try {
+                choix.setCurrentDirectory(new File("./datas"));
+                if (choix.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    pathPlan = choix.getSelectedFile().getAbsolutePath();
+                    chargerPlan(pathPlan);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+        controller.chargerDemande.setOnAction(event -> {
+            String pathDemande = "";
+            try {
+                choix.setCurrentDirectory(new File("./datas"));
+                if (choix.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    pathDemande = choix.getSelectedFile().getAbsolutePath();
+                }
+                chargerDemande(pathDemande);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
 
 
     }
 
-    
-}
+
+
 
 
 /*    @Override
@@ -58,16 +80,18 @@ public class Listener {
 
         }
 
-    }
+    }*/
 
-    public void chargerPlan( String path) throws Exception {
-        ArrayList<Coordinate> limites=new ArrayList<Coordinate>();
-        limites= service.chargerPlan(path);
+    public void chargerPlan(String path) throws Exception {
+        ArrayList<Coordinate> limites = new ArrayList<Coordinate>();
+        limites = service.chargerPlan(path);
         controller.setMapExtent(limites);
     }
 
     public void chargerDemande(String path) throws Exception {
-        Demande d= service.chargerDemande(path);
+        Demande d = service.chargerDemande(path);
         controller.chargerDemande(d);
     }
-} */
+}
+
+
