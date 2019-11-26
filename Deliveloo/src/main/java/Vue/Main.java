285cf4ffba3dba7e31a510da9d00b826e42f47c6
jeanhbr <jean.haberer@bde-insa-lyon.fr>
2019-11-26 11:35:49 +0100

    Vue qui marche

--- Deliveloo/src/main/java/Vue/Main.java
+++ Deliveloo/src/main/java/Vue/Main.java
@@ -1,4 +1,35 @@
 package Vue;
 
-public class Main {
+import com.sothawo.mapjfx.Projection;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Demo application for the mapjfx component.
+ *
+ * @author P.J. Meisch (pj.meisch@sothawo.com).
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        String fxmlFile = "/VueDeliveloo.fxml";
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent rootNode = fxmlLoader.load(getClass().getResourceAsStream(fxmlFile));
+
+        Scene scene = new Scene(rootNode);
+
+        primaryStage.setTitle("sothawo mapjfx demo application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
 }
