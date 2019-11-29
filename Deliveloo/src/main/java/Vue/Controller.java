a552fdf7d85bf417579a4af15554965ecbf8a91e
manalelka <45950280+manalelka@users.noreply.github.com>
2019-11-29 09:36:51 +0100

    LOGOOO TROP GRAND????

--- Deliveloo/src/main/java/Vue/Controller.java
+++ Deliveloo/src/main/java/Vue/Controller.java
@@ -341,7 +341,8 @@ public class Controller {
                 for (int i = 0; i < demande.getLivraisons().size(); i++) {
                     Marker markerPickUp;
                     Coordinate pickUp = demande.getLivraisons().get(i).getPickup().getCoordinate();
-                    markerPickUp = Marker.createProvided(Marker.Provided.BLUE).setPosition(pickUp);
+                    URL imageURL = new URL("file:///C:/Users/manal/Documents/GitHub/Agile/datas/logos/pick_up_logo.png");
+                    markerPickUp = new Marker(imageURL, 0, 0).setPosition(pickUp);
 
                     Marker markerDelivery;
                     Coordinate delivery = demande.getLivraisons().get(i).getDelivery().getCoordinate();
