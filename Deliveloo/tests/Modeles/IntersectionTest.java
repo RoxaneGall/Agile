b9c98abdc4d6c826473680240c4abb562ade6411
Ahmed Abdel Rahman <ahmed.abdel_rahman@hotmail.com>
2019-11-29 10:22:51 +0100

    getTroncons test

--- Deliveloo/tests/Modeles/IntersectionTest.java
+++ Deliveloo/tests/Modeles/IntersectionTest.java
@@ -37,18 +37,26 @@ class IntersectionTest {
         Coordinate p = new Coordinate(4.112233, 5.32404);
         long initialId = 34;
         Intersection inter = new Intersection(initialId,p);
-    }
+        Troncon t = new Troncon(inter, "rue ahmed", 12.243);
 
-    @Test
-    void getP() {
-    }
+        //Act
+        inter.addTroncon(t);
 
-    @Test
-    void setP() {
+        //Assert
+        assertTrue(inter.getTroncons().size() instanceof int);
+        assertEquals(1,inter.getTroncons().size());
+
     }
 
     @Test
     void getTroncons() {
+        //Arrange
+        Coordinate p = new Coordinate(4.112233, 5.32404);
+        long initialId = 34;
+        Intersection inter = new Intersection(initialId,p);
+
+        //Act
+        inter.getTroncons();
     }
 
     @Test
