e0cc6f89a9bf958ea4da5fafcdf3ef90fb702372
Ahmed Abdel Rahman <ahmed.abdel_rahman@hotmail.com>
2019-11-30 19:47:13 +0100

    fin tests

--- Deliveloo/tests/Modeles/IntersectionTest.java
+++ Deliveloo/tests/Modeles/IntersectionTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IntersectionTest {
@@ -43,23 +45,23 @@ class IntersectionTest {
         inter.addTroncon(t);
 
         //Assert
-        assertTrue(inter.getTroncons().size() instanceof int);
         assertEquals(1,inter.getTroncons().size());
 
     }
 
     @Test
-    void getTroncons() {
+    void getTronconsTest_ShouldReturnTronconsMap() {
         //Arrange
         Coordinate p = new Coordinate(4.112233, 5.32404);
         long initialId = 34;
         Intersection inter = new Intersection(initialId,p);
+        Troncon t = new Troncon(inter, "rue ahmed", 12.243);
+        inter.addTroncon(t);
 
         //Act
-        inter.getTroncons();
-    }
+        Collection c = inter.getTroncons();
 
-    @Test
-    void testToString() {
+        //Assert
+        assertEquals(1,c.size());
     }
 }
