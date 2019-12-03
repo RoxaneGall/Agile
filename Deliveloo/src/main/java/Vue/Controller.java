602db1aa0bb5fbda1963b167067298419d2725f1
manalelka <45950280+manalelka@users.noreply.github.com>
2019-12-03 09:19:44 +0100

    Update

--- Deliveloo/src/main/java/Vue/Controller.java
+++ Deliveloo/src/main/java/Vue/Controller.java
@@ -269,6 +269,7 @@ public class Controller {
 
     private void chargerPlan() {
         System.out.println("Chargement du plan");
+        choix.setCurrentDirectory(new File("../datas/"));
         try {
             ArrayList<Coordinate> limites = service.chargerPlan("../datas/grandPlan.xml");
 
