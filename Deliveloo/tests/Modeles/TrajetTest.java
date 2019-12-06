437631bea9054c9a3908254996b76c0fff7decbf
Ahmed Abdel Rahman <ahmed.abdel_rahman@hotmail.com>
2019-12-06 09:30:11 +0100

    Tests trajet

--- Deliveloo/tests/Modeles/TrajetTest.java
+++ Deliveloo/tests/Modeles/TrajetTest.java
@@ -4,6 +4,8 @@ import com.sothawo.mapjfx.Coordinate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TrajetTest {
@@ -40,7 +42,7 @@ class TrajetTest {
     }
 
     @Test
-    void getOrigineTest_ShouldReturnOriginIntersection() {
+    void getOrigineTest_ShouldReturnOrigineIntersection() {
         //Arrange
         Intersection i1 = new Intersection(1,new Coordinate(0.0,0.0));
         Intersection i2 = new Intersection(2,new Coordinate(0.0,3.0));
@@ -61,17 +63,16 @@ class TrajetTest {
         trajet.addTroncon(t45);
         trajet.addTroncon(t51);
 
-        Double expectedLong = t12.getLongueur()+ t23.getLongueur() + t34.getLongueur() + t45.getLongueur() + t51.getLongueur();
-
         //Act
-        Double actualLong = trajet.getLongueur();
+        Intersection actualO = trajet.getOrigine();
 
-        //Assert
-        assertEquals(expectedLong, actualLong);
+        //Act & Assert
+        assertTrue(actualO instanceof Intersection);
+        assertEquals(i1,actualO);
     }
 
     @Test
-    void getInstructionsTest_ShouldReturnTronconListWithCorrectSize() {
+    void getArriveeTest_ShouldReturnArriveeIntersection() {
         //Arrange
         Intersection i1 = new Intersection(1,new Coordinate(0.0,0.0));
         Intersection i2 = new Intersection(2,new Coordinate(0.0,3.0));
@@ -93,10 +94,149 @@ class TrajetTest {
         trajet.addTroncon(t51);
 
         //Act
-        int actualSize = trajet.getInstructions().size();
+        Intersection actualO = trajet.getOrigine();
 
-        //Assert
-        assertEquals(4,actualSize);
+        //Act & Assert
+        assertTrue(actualO instanceof Intersection);
+        assertEquals(i1,actualO);
     }
 
+    @Test
+    void getTronconsTest_ShouldReturnTronconListWithCorrectSize() {
+        //Arrange
+        Intersection i1 = new Intersection(1,new Coordinate(0.0,0.0));
+        Intersection i2 = new Intersection(2,new Coordinate(0.0,3.0));
+        Intersection i3 = new Intersection(3,new Coordinate(3.0,3.0));
+        Intersection i4 = new Intersection(4,new Coordinate(3.0,6.0));
+        Intersection i5 = new Intersection(5,new Coordinate(3.0,12.0));
+
+        Troncon t12 = new Troncon(i2,"",2.0);
+        Troncon t23 = new Troncon(i3,"",2.0);
+        Troncon t34 = new Troncon(i4,"",2.0);
+        Troncon t45 = new Troncon(i5,"",2.0);
+        Troncon t51 = new Troncon(i1,"",2.0);
+
+        ArrayList<Troncon> ts = new ArrayList<>();
+        ts.add(t12);
+        ts.add(t23);
+        ts.add(t34);
+        ts.add(t45);
+        ts.add(t51);
+
+        Trajet trajet = new Trajet(i1);
+        trajet.addTroncons(ts);
+
+        //Act
+        ArrayList<Troncon> actualT = trajet.getTroncons();
+
+        //Assert
+        int i = 0;
+        for(Troncon t : ts) {
+            assertTrue(actualT.toArray()[i] instanceof Troncon);
+            assertEquals(t, actualT.toArray()[i]);
+            i++;
+        }
+    }
+
+    @Test
+    void addTronconTest_ShouldIncreaseTronconsListSize() {
+        //Arrange
+        Intersection i1 = new Intersection(1,new Coordinate(0.0,0.0));
+        Intersection i2 = new Intersection(2,new Coordinate(0.0,3.0));
+        Intersection i3 = new Intersection(3,new Coordinate(3.0,3.0));
+        Intersection i4 = new Intersection(4,new Coordinate(3.0,6.0));
+        Intersection i5 = new Intersection(5,new Coordinate(3.0,12.0));
+
+        Troncon t12 = new Troncon(i2,"",2.0);
+        Troncon t23 = new Troncon(i3,"",2.0);
+        Troncon t34 = new Troncon(i4,"",2.0);
+        Troncon t45 = new Troncon(i5,"",2.0);
+        Troncon t51 = new Troncon(i1,"",2.0);
+
+        ArrayList<Troncon> ts = new ArrayList<>();
+        ts.add(t12);
+        ts.add(t23);
+        ts.add(t34);
+        ts.add(t45);
+        ts.add(t51);
+
+        Trajet trajet = new Trajet(i1);
+
+        //Act
+        trajet.addTroncon(t12);
+
+        //Assert
+        assertEquals(1,trajet.getTroncons().size());
+        assertEquals(t12,trajet.getTroncons().get(0));
+    }
+
+    @Test
+    void addTronconsTest_ShouldIncreaseTronconsListSize() {
+        //Arrange
+        Intersection i1 = new Intersection(1,new Coordinate(0.0,0.0));
+        Intersection i2 = new Intersection(2,new Coordinate(0.0,3.0));
+        Intersection i3 = new Intersection(3,new Coordinate(3.0,3.0));
+        Intersection i4 = new Intersection(4,new Coordinate(3.0,6.0));
+        Intersection i5 = new Intersection(5,new Coordinate(3.0,12.0));
+
+        Troncon t12 = new Troncon(i2,"",2.0);
+        Troncon t23 = new Troncon(i3,"",2.0);
+        Troncon t34 = new Troncon(i4,"",2.0);
+        Troncon t45 = new Troncon(i5,"",2.0);
+        Troncon t51 = new Troncon(i1,"",2.0);
+
+        ArrayList<Troncon> ts = new ArrayList<>();
+        ts.add(t12);
+        ts.add(t23);
+        ts.add(t34);
+        ts.add(t45);
+        ts.add(t51);
+
+        Trajet trajet = new Trajet(i1);
+
+        //Act
+        trajet.addTroncons(ts);
+
+        //Assert
+        ArrayList<Troncon> actualT = trajet.getTroncons();
+        assertEquals(ts.size(),actualT.size());
+        int i = 0;
+        for(Troncon t : ts) {
+            assertTrue(actualT.toArray()[i] instanceof Troncon);
+            assertEquals(t, actualT.toArray()[i]);
+            i++;
+        }
+    }
+
+    @Test
+    void getInstructionsTest_ShouldReturnCorrectAngleValue() {
+        //Arrange
+        Intersection i1 = new Intersection(1,new Coordinate(0.0,0.0));
+        Intersection i2 = new Intersection(2,new Coordinate(0.0,3.0));
+        Intersection i3 = new Intersection(3,new Coordinate(3.0,3.0));
+        Intersection i4 = new Intersection(4,new Coordinate(3.0,6.0));
+        Intersection i5 = new Intersection(5,new Coordinate(3.0,12.0));
+
+        Troncon t12 = new Troncon(i2,"",2.0);
+        Troncon t23 = new Troncon(i3,"",2.0);
+        Troncon t34 = new Troncon(i4,"",2.0);
+        Troncon t45 = new Troncon(i5,"",2.0);
+        Troncon t51 = new Troncon(i1,"",2.0);
+
+        ArrayList<Troncon> ts = new ArrayList<>();
+        ts.add(t12);
+        ts.add(t23);
+        ts.add(t34);
+        ts.add(t45);
+        ts.add(t51);
+
+        Trajet trajet = new Trajet(i1);
+        trajet.addTroncons(ts);
+
+        //Act
+        ArrayList<InstructionLivraison> instr = trajet.getInstructions();
+
+        //Assert
+        assertNotNull(instr);
+    }
 }
