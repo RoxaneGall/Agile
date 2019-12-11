5bf8d7914a2e922eb31790d6dc9e2914eb3a94c2
manalelka <45950280+manalelka@users.noreply.github.com>
2019-12-11 14:24:31 +0100

    Update Controller.java

--- Deliveloo/src/main/java/Vue/Controller.java
+++ Deliveloo/src/main/java/Vue/Controller.java
@@ -555,7 +555,7 @@ public class Controller implements ActionListener {
                         mapView.addMarker(entry.getValue().setVisible(true));
                     }
 
-                    System.out.println(deliveriesMarkers.size());
+                    System.out.println(demande.getLivraisons().size());
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
