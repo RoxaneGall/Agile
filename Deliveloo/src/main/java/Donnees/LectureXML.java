487eaef18222ab75701c8a294103bb738bc3e0a8
jeanhbr <jean.haberer@bde-insa-lyon.fr>
2019-12-13 09:52:50 +0100

    Voila

--- Deliveloo/src/main/java/Donnees/LectureXML.java
+++ Deliveloo/src/main/java/Donnees/LectureXML.java
@@ -156,8 +156,8 @@ Faudrait que tu renvoies à l'IHM des trucs différents selon l'erreur ou qu'on 
         Intersection entrepot = new Intersection();
 
         Long countDeliveries= new Long(0);
-        String nomDemande = cheminFichier.substring(cheminFichier.lastIndexOf("/"),cheminFichier.lastIndexOf("."));
-        Demande demande = new Demande(entrepot, myDate,nomDemande);
+        String nomDemande = cheminFichier.substring(cheminFichier.replace("\\","/").lastIndexOf("/"),cheminFichier.lastIndexOf("."));
+        Demande demande = new Demande(entrepot, myDate, nomDemande);
         for(int i=0; i<nbRootNodes; i++){
             Node node = rootNodes.item(i);
             NamedNodeMap attributes = node.getAttributes();
