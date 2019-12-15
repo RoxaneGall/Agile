cfe6a7ca0432324f90bda87dd7f0b91b2bcd3225
Ahmed Abdel Rahman <ahmed.abdel_rahman@hotmail.com>
2019-12-15 17:51:39 +0100

    Javadoc

--- Deliveloo/src/main/java/Modeles/Demande.java
+++ Deliveloo/src/main/java/Modeles/Demande.java
@@ -6,46 +6,85 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
-
+/**
+ * Classe representant une demande de tournee. Contient une liste des livraisons demandees, la localisation de l'entrepot et l'heure de depart.
+ */
 public class Demande {
     private ArrayList<Livraison> livraisons;
     private Intersection entrepot;
     private Date heureDepart;
 
+    /**
+     *
+     * @param entrepot
+     * @param heureDepart
+     */
     public Demande(Intersection entrepot, Date heureDepart) {
         this.livraisons = new ArrayList<>();
         this.entrepot = entrepot;
         this.heureDepart = heureDepart;
     }
 
+    /**
+     *
+     * @return les livraisons demandees
+     */
     public ArrayList<Livraison> getLivraisons() {
         return livraisons;
     }
 
+    /**
+     * ajoute une livraison a la demande
+     * @param pickup
+     * @param delivery
+     * @param dureeEnlevement
+     * @param dureeLivraison
+     * @return
+     */
     public Livraison addLivraison(Intersection pickup,  Intersection delivery, int dureeEnlevement, int dureeLivraison) {
         Livraison livraison = new Livraison( ((Integer) livraisons.size()).longValue(), pickup, delivery, dureeEnlevement, dureeLivraison);
         livraisons.add(livraison);
         return livraison;
     }
 
+    /**
+     * ajoute une liste de livraisons a la demande
+     * @param livraisons
+     */
     public void addLivraisons(ArrayList<Livraison> livraisons) {
         for (Livraison livraison: livraisons) {
             addLivraison(livraison.getPickup(), livraison.getDelivery(), livraison.dureeEnlevement, livraison.getDureeLivraison());
         }
     }
 
+    /**
+     *
+     * @return l'entrepot de la demande
+     */
     public Intersection getEntrepot() {
         return entrepot;
     }
 
+    /**
+     * modifie l'entrepot de la demande
+     * @param entrepot
+     */
     public void setEntrepot(Intersection entrepot) {
         this.entrepot = entrepot;
     }
 
+    /**
+     *
+     * @return l'heure de depart de la tournee qui sera calculee
+     */
     public Date getHeureDepart() {
         return heureDepart;
     }
 
+    /**
+     * modifie l'heure de depart de la tournee qui sera calculee
+     * @param heureDepart
+     */
     public void setHeureDepart(Date heureDepart) {
         this.heureDepart = heureDepart;
     }
