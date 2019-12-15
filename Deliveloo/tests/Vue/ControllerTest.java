c4b5f1f4bf2d3efaf694c6fbfd5f3ca2b6108ec6
manalelka <45950280+manalelka@users.noreply.github.com>
2019-12-15 15:23:07 +0100

    Tests

--- Deliveloo/tests/Vue/ControllerTest.java
+++ Deliveloo/tests/Vue/ControllerTest.java
@@ -1,15 +1,16 @@
 package Vue;
 
 import Modele.Demande;
+import Modele.Tournee;
+import Modele.Trajet;
+import Modele.Troncon;
 import Service.Service;
-import com.sothawo.mapjfx.Coordinate;
-import com.sothawo.mapjfx.Extent;
-import com.sothawo.mapjfx.Marker;
-import com.sothawo.mapjfx.Projection;
+import com.sothawo.mapjfx.*;
 import com.sun.javafx.application.ParametersImpl;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Pair;
 import org.junit.jupiter.api.Assertions;
@@ -18,6 +19,8 @@ import javafx.application.Application;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Queue;
+import java.util.stream.Collectors;
 
 class ControllerTest {
     @Test
@@ -45,17 +48,17 @@ class ControllerTest {
 
     @Test
     public void markersPositionTest() throws Exception {
-
-        ArrayList<Pair<Marker, Marker>> deliveriesMarkers = new ArrayList<Pair<Marker, Marker>>();
         Service service = new Service();
         service.chargerPlan("../datas/grandPlan.xml");
-        String path = "../datas/demandeGrand7.xml";
+        String path = "../datas/demandeGrand9.xml";
+        ArrayList<Pair<Marker, Marker>> deliveriesMarkers = new ArrayList<Pair<Marker, Marker>>();
         Demande demande = service.chargerDemande(path);
         for (int i = 0; i < demande.getLivraisons().size(); i++) {
             Coordinate pickUp = demande.getLivraisons().get(i).getPickup().getCoordinate();
             Coordinate delivery = demande.getLivraisons().get(i).getDelivery().getCoordinate();
             Marker markerPickUp = Marker.createProvided(Marker.Provided.BLUE).setPosition(pickUp);
-            Marker markerDelivery = Marker.createProvided(Marker.Provided.BLUE).setPosition(delivery);;
+            Marker markerDelivery = Marker.createProvided(Marker.Provided.BLUE).setPosition(delivery);
+            ;
             deliveriesMarkers.add(new Pair<Marker, Marker>(markerPickUp, markerDelivery));
         }
         for (int i = 0; i < deliveriesMarkers.size(); i++) {
@@ -67,6 +70,42 @@ class ControllerTest {
 
         System.out.println(" Deliveries markers position Test : PASSED ");
 
+
+    }
+
+    @Test
+    public void coordinateLinePositionTest() throws Exception {
+        Service service = new Service();
+        service.chargerPlan("../datas/grandPlan.xml");
+        String path = "../datas/demandeGrand9.xml";
+        Demande demande = service.chargerDemande(path);
+        service.calculerTournee(demande);
+        Tournee t = service.recupererTournee();
+        ArrayList<Coordinate> tourneeCoordinate = new ArrayList<>();
+
+        for (int i = 0; i < t.getTrajets().size(); i++) {
+            Trajet trajet = t.getTrajets().get(i);
+            Coordinate origine = trajet.getOrigine().getCoordinate();
+            tourneeCoordinate.add(origine);
+            for (Troncon troncon : t.getTrajets().get(i).getTroncons()) {
+                tourneeCoordinate.add(troncon.getDestination().getCoordinate());
+            }
+        }
+        CoordinateLine trackTrajet = new CoordinateLine(tourneeCoordinate).setColor(Color.DARKRED).setWidth(8);
+        ArrayList<Coordinate> tab = (ArrayList<Coordinate>) trackTrajet.getCoordinateStream().collect(Collectors.toList());
+        System.out.println(tab.size());
+        System.out.println(t.getTrajets().size());
+        int i = 0;
+        while (i < t.getTrajets().size()) {
+            Assertions.assertEquals(tab.get(i), t.getTrajets().get(i).getOrigine().getCoordinate());
+            int j = i + 1;
+            for (Troncon troncon : t.getTrajets().get(i).getTroncons()) {
+                Assertions.assertEquals(tab.get(j), troncon.getDestination().getCoordinate());
+                j++;
+                i = j;
+            }
+        }
+        System.out.println(" Coordinate line position Test : PASSED ");
 
     }
 
